596dc723cc07cde63a62c7ee109540995d88c0e0
Ivan Seibel <me@ivanseibel.dev>
2022-12-13 23:38:53 +0000

    add modal to header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,8 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
+import * as Dialog from '@radix-ui/react-dialog'
 
 import logo from '../../assets/logo.png'
+import { NewTransactionModal } from '../NewTransactionModal'
 
 export function Header() {
   return (
@@ -11,7 +13,13 @@ export function Header() {
           <strong>myWallet</strong>
         </div>
 
-        <NewTransactionButton>New transaction</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>New transaction</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
