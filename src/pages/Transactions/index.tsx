0691d869aa9067f901e19aef2fdb53a4472a4ab5
Ivan Seibel <me@ivanseibel.dev>
2022-12-13 22:11:19 +0000

    create table component

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,13 +1,48 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
 
-import { TransactionsContainer } from './styles'
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
 
 export function Transactions() {
   return (
-    <TransactionsContainer>
+    <div>
       <Header />
       <Summary />
-    </TransactionsContainer>
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Software development</td>
+              <td>
+                <PriceHighlight variant="income">$1,000.00</PriceHighlight>
+              </td>
+              <td>Contract</td>
+              <td>2022-12-02</td>
+            </tr>
+            <tr>
+              <td width="50%">Tesco Supermarket</td>
+              <td>
+                <PriceHighlight variant="outcome">- $100.00</PriceHighlight>
+              </td>
+              <td>Grocery</td>
+              <td>2022-12-03</td>
+            </tr>
+            <tr>
+              <td width="50%">Rocketseat subscription</td>
+              <td>
+                <PriceHighlight variant="outcome">- $300.00</PriceHighlight>
+              </td>
+              <td>Subscription</td>
+              <td>2022-12-07</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
   )
 }
